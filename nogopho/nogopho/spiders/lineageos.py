d4689adfddc28360ba7da5b6d7d88588630c37e3
leoossa <leonard@leonardossa.com>
2021-07-24 21:32:52 +0200

    Simple scraping lineageos wiki devices list

--- nogopho/nogopho/spiders/lineageos.py
+++ nogopho/nogopho/spiders/lineageos.py
@@ -3,8 +3,15 @@ import scrapy
 
 class LineageosSpider(scrapy.Spider):
     name = 'lineageos'
-    allowed_domains = ['https://wiki.lineageos.org/devices/']
-    start_urls = ['http://https://wiki.lineageos.org/devices//']
+    allowed_domains = ['wiki.lineageos.org']
+    start_urls = ['https://wiki.lineageos.org/devices/']
 
     def parse(self, response):
+        print("HTTP Status Code:", response.status)
+        devices = response.css('th[scope="row"]')
+        for device in devices:
+            smartphone = device.css('a')
+            name = smartphone.css('a::text').get()
+            link = smartphone.css('a::attr(href)').get()
+            print("Device:", name, "link", link)
         pass
