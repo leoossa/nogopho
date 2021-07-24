fe63e2ac72a55e1868f6cda9a9b76055beb5f576
leoossa <leonard@leonardossa.com>
2021-07-25 01:07:52 +0200

    Add scraping name,supported versions, release date

--- nogopho/nogopho/spiders/lineageos.py
+++ nogopho/nogopho/spiders/lineageos.py
@@ -7,11 +7,21 @@ class LineageosSpider(scrapy.Spider):
     start_urls = ['https://wiki.lineageos.org/devices/']
 
     def parse(self, response):
-        print("HTTP Status Code:", response.status)
-        devices = response.css('th[scope="row"]')
-        for device in devices:
-            smartphone = device.css('a')
-            name = smartphone.css('a::text').get()
-            link = smartphone.css('a::attr(href)').get()
-            print("Device:", name, "link", link)
-        pass
+        devices_links = response.css('th[scope="row"] a')
+        yield from response.follow_all(devices_links, self.parse_device)
+
+    def parse_device(self, response):
+        # First table row (<tr>) is name of the device
+        name = response.css('table.deviceinfo tr th::text').get()
+        released = response.xpath(
+            '//tr[th[contains(text(), "Released")]]//td/text()').getall()
+        released = [release.strip()
+                    for release in released]  # Strip newlines and spaces
+        supported_versions = response.xpath(
+            '//tr[th[contains(text(), "versions")]]//li/text()').getall()
+        # Last table row <tr> is <ul><li>...</ul> for supported versions if there are supported versions
+        # if it is not maintained - last table row is 'Battery' and there's no <ul> and no <li>
+        if len(supported_versions) == 0:
+            supported_versions = 'unmaintained'
+
+        yield{'name': name, 'supported_versions': supported_versions, 'released': released}
